ff68a2f872c3bf11e8f60732eef74889858821c1
Hossein <138606480+h3nrzi@users.noreply.github.com>
2026-02-06 02:01:03 +0330

    Add RTL stylesheet and font support

--- inc/actions/theme/enqueue-scripts.php
+++ inc/actions/theme/enqueue-scripts.php
@@ -16,12 +16,20 @@ function university_files(): void {
 	// Fonts
 	wp_enqueue_style( 'custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
 
+	if ( is_rtl() ) {
+		wp_enqueue_style( 'custom-rtl-fonts', 'https://fonts.googleapis.com/css2?family=Vazirmatn:wght@300;400;500;700&display=swap', [], null );
+	}
+
 	// Icons
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
 
 	// Styles
 	wp_enqueue_style( 'university-main-styles', get_theme_file_uri( '/build/style-index.css' ) );
 	wp_enqueue_style( 'university-extra-styles', get_theme_file_uri( '/build/index.css' ) );
+
+	if ( is_rtl() ) {
+		wp_enqueue_style( 'university-rtl-styles', get_theme_file_uri( '/rtl.css' ) );
+	}
 }
 
 add_action( 'wp_enqueue_scripts', 'university_files' );
